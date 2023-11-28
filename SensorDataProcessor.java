6bd05fdc739d0bf19f639fa6aaecd148da91c7e6
OsamaAlmalki2042178 <146426709+OsamaAlmalki2042178@users.noreply.github.com>
2023-11-28 22:36:12 +0300

    Update SensorDataProcessor.java

--- SensorDataProcessor.java
+++ SensorDataProcessor.java
@@ -21,37 +21,37 @@ public class SensorDataProcessor { // changed file name to suit the class name {
         return sum / array.length;
     }
 
-    // calculate data
-    public void calculate(double d) {
+// calculate data {Osama 25-50}
+public void calculate(double d) {
+    int i, j, k;
+    double[][][] data2 = new double[data.length][data[0].length][data[0][0].length];
 
-        int i, j, k = 0;
-        double[][][] data2 = new double[data.length][data[0].length][data[0][0].length];
+    BufferedWriter out;
+    // Write racing stats data into a file
+    try {
+        out = new BufferedWriter(new FileWriter("RacingStatsData.txt"));
 
-        BufferedWriter out;
-        // Write racing stats data into a file
-        try {
-            out = new BufferedWriter(new FileWriter("RacingStatsData.txt"));
+        for (i = 0; i < data.length; i++) {
+            for (j = 0; j < data[0].length; j++) {
+                double avgData2 = average(data2[i][j]); 
+                for (k = 0; k < data[0][0].length; k++) {
+                    data2[i][j][k] = data[i][j][k] / d - Math.pow(limit[i][j], 2.0);
 
-            for (i = 0; i < data.length; i++) {
-                for (j = 0; j < data[0].length; j++) {
-                    for (k = 0; k < data[0][0].length; k++) {
-                        data2[i][j][k] = data[i][j][k] / d -
-                                Math.pow(limit[i][j], 2.0);
-
-                        if (average(data2[i][j]) > 10 && average(data2[i][j]) < 50)
-                            break;
-                        else if (Math.max(data[i][j][k], data2[i][j][k]) > data[i][j][k])
-                            break;
-                        else if (Math.pow(Math.abs(data[i][j][k]), 3) < Math.pow(Math.abs(data2[i][j][k]), 3)
-                                && average(data[i][j]) < data2[i][j][k] && (i + 1)
-                                        * (j + 1) > 0)
-                            data2[i][j][k] *= 2;
-                        else
-                            continue;
-                    }
+                    if (avgData2 > 10 && avgData2 < 50)
+                        break;
+                    else if (Math.max(data[i][j][k], data2[i][j][k]) > data[i][j][k])
+                        break;
+                    else if (Math.pow(Math.abs(data[i][j][k]), 3) < Math.pow(Math.abs(data2[i][j][k]), 3)
+                            && avgData2 < data2[i][j][k] && (i + 1) * (j + 1) > 0)
+                        data2[i][j][k] *= 2;
+                   
+                }
                 }
             }
 
+
+
+        
             for (double[][] datum2 : data2) {
                 for (double[] doubles : datum2) {
                     for (double aDouble : doubles) {
